7b3fff36ca29723d761e5dd0479d08dd1d54d08e
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-08-28 19:40:50 +0530

    aiService gemini api

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -3,6 +3,7 @@ package com.fitness.aiservice.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
